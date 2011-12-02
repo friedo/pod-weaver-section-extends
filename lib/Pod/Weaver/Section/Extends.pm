b8bdc3bcb7803a37aa9d97036b2b781891f55ca1
Mike Friedman <friedo@friedo.com>
2011-12-02 00:28:41 -0500

    added docs, bumped version

--- lib/Pod/Weaver/Section/Extends.pm
+++ lib/Pod/Weaver/Section/Extends.pm
@@ -13,8 +13,6 @@ use aliased 'Pod::Elemental::Element::Nested';
 use aliased 'Pod::Elemental::Element::Pod5::Verbatim';
 use aliased 'Pod::Elemental::Element::Pod5::Command';
 
-use Data::Dumper;
-
 sub weave_section { 
     my ( $self, $doc, $input ) = @_;
 
@@ -73,3 +71,19 @@ sub _get_parents {
 
 
 1;
+
+
+=pod
+
+=head1 SYNOPSIS
+
+In your C<weaver.ini>:
+
+    [Extends]
+
+=head1 DESCRIPTION
+
+This L<Pod::Weaver> section plugin creates an "EXTENDS" section in your POD
+which will contain a list of your class's parent classes. It accomplishes
+this by attempting to compile your class and inspecting its C<@ISA>. 
+
